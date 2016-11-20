230dce5d9c749c8ab6f19c385311ff2a7a49f6af
Ativut Trikalsaransukh <ativut.trikalsaransukh@thomsonreuters.com>
2016-11-20 12:31:12 +0700

    init project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name msWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the msWebApp
+ */
+angular.module('msWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name msWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the msWebApp
+ */
+angular.module('msWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
