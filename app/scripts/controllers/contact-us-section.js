b092d244292b0d354283cb52aa137ecc5076e4a1
Ativut Trikalsaransukh <ativut@gmail.com>
2016-12-31 03:19:18 +0700

    add google maps

--- app/scripts/controllers/contact-us-section.js
+++ app/scripts/controllers/contact-us-section.js
@@ -2,16 +2,33 @@
 
 angular.module('msWebApp')
 
-  .controller('ContactUsSectionCtrl', function () {
+  .controller('ContactUsSectionCtrl', [ function () {
 
-    };
-  })
+  }])
 
-  .directive('contactUsSection', function () {
+  .directive('contactUsSection', ['$window', function ($window) {
     return {
       restrict: 'E',
       controller: 'ContactUsSectionCtrl',
       templateUrl: 'views/contact-us-section.html',
-      replace: true
+      replace: true,
+      link: function () {
+        function initMap () {
+          var latlng = new google.maps.LatLng(13.7195362, 100.5806025);
+          var myOptions = {
+              zoom: 17,
+              center: latlng,
+              scrollwheel:  true
+          };
+
+          var map = new google.maps.Map($window.document.getElementById('map'), myOptions);
+          var marker = new google.maps.Marker({
+            position: {lat: 13.719627, lng: 100.580466},
+            map: map
+          });
+        }
+
+        initMap();
+      }
     };
-  });
+  }]);
